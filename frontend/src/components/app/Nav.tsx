f0bee8d44e7e13a4b8d4946c00850284a06a5222
valerofin <valerofin.111@gmail.com>
2025-12-16 18:21:12 +0300

    Add navigation and themes for Card component

---
+++ frontend/src/components/app/Nav.tsx
@@ -0,0 +1,38 @@
+import { Flex } from '@radix-ui/themes'
+import * as Select from '@radix-ui/react-select'
+import { useNavigate } from 'react-router-dom'
+import styles from '../../styles/app.module.scss'
+
+var Nav = (
+    ({className} : string) => {
+
+        var nav = useNavigate()
+
+        return (
+            <>
+                <Flex justify={'center'}>
+                    <Select.Root onValueChange={(value) => {
+                        nav(`${value}`)
+                    }}>
+                        <Select.Trigger>
+                            Nav
+                        </Select.Trigger>
+                        <Select.Portal>
+                            <Select.Content className={styles[className]} position='popper'>
+                                <Select.Viewport>
+                                    <Select.Group>
+                                        <Select.Item value='chat'>Chat</Select.Item>
+                                        <Select.Item value='friends'>Friends</Select.Item>
+                                        <Select.Item value='options'>Options</Select.Item>
+                                    </Select.Group>
+                                </Select.Viewport>
+                            </Select.Content>
+                        </Select.Portal>
+                    </Select.Root>
+                </Flex>
+            </>
+        )
+    }
+) 
+
+export default Nav
