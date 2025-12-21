c2cb1900b9ed6e33d8a8e48cd78cb59c89703416
valerofin <valerofin.111@gmail.com>
2025-12-21 14:32:33 +0300

    Minor changes: add request to backend(find possible friends)

--- frontend/src/components/app/mainCard/SearchFriends.tsx
+++ frontend/src/components/app/mainCard/SearchFriends.tsx
@@ -4,11 +4,33 @@ import { Flex } from "@radix-ui/themes"
 import PickText from "../../library/PickText/PickText"
 import themeAtom from "../../../atoms/themeAtom"
 import { useAtomValue } from "jotai"
+import { useQuery } from "@tanstack/react-query"
+import Error from "../../fallback/Error"
+import Loading from "../../fallback/Loading"
+import axios from "axios"
+
+var posFriendsFetch = async () => {
+    try {
+        var response = await axios.get('http://localhost:5000/friends?search=val')
+        return response.data
+    } catch (e) {
+        console.error(e)
+        throw e
+    }
+}
 
 var SearchFriends = () => {
 
+    var { data, isError, isLoading } = useQuery({
+        queryKey: ['posibleFriends'],
+        queryFn: posFriendsFetch
+    })
+
     var theme = useAtomValue(themeAtom)
 
+    if (isError) return <Error />
+    if (isLoading) return <Loading />
+    
     return (
         <>
             <Flex justify={'center'} >
@@ -16,14 +38,11 @@ var SearchFriends = () => {
             </Flex>
                 
             <Flex direction={'column'} >
-                <p>Friend</p>
-                <p>Friend</p>
-                <p>Friend</p>
-                <p>Friend</p>
-                <p>Friend</p>
-                <p>Friend</p>
-                <p>Friend</p>
-                <p>Friend</p>
+            {
+                data.map(friend => (
+                    <p key={friend.name} >{friend.name} </p>
+                ))
+            }
             </Flex>
         </>
     )
